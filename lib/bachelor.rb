b8da4761ab58f7465c0d11f9ee638b77bf7c9815
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-11-27 23:56:34 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -32,10 +32,10 @@ def get_contestant_name(data, occupation)
 end
 
 
-# def count_contestants_by_hometown(data, hometown)
-#   # code here
-# end
-#
+ def count_contestants_by_hometown(data, hometown)
+   counter = 0
+ end
+
 # def get_occupation(data, hometown)
 #   # code here
 # end
