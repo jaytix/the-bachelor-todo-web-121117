18c5b54fd8988ba3d2efa41ece9ad316a469d24c
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-11-27 23:19:37 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,21 +1,21 @@
 def get_first_name_of_season_winner(data, season)
   data.season.each do |contestants|
     binding.pry
-  end  
+  end
 end
 
-def get_contestant_name(data, occupation)
-  # code here
-end
-
-def count_contestants_by_hometown(data, hometown)
-  # code here
-end
-
-def get_occupation(data, hometown)
-  # code here
-end
-
-def get_average_age_for_season(data, season)
-  # code here
-end
+# def get_contestant_name(data, occupation)
+#   # code here
+# end
+#
+# def count_contestants_by_hometown(data, hometown)
+#   # code here
+# end
+#
+# def get_occupation(data, hometown)
+#   # code here
+# end
+#
+# def get_average_age_for_season(data, season)
+#   # code here
+# end
