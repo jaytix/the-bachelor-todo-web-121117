4b03594fb70605f45627e0173d4ed21d35ec01f4
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-11-28 00:07:46 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -62,7 +62,7 @@ def get_average_age_for_season(data, season)
   counter = 0
   data.keys.each do |seasons|
     data[seasons].each do |contestant|
-      counter += contestant.values[1].to_s
+      counter += contestant.values[1].to_i
     end
   end
   counter / data[seasons].length
