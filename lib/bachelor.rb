bd9b8958a3857b7c0f23a56a5ce78a4f9600116b
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-11-28 00:04:26 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -63,6 +63,7 @@ def get_average_age_for_season(data, season)
     data[seasons].each do |contestant|
       contestant.values.each do |stats|
         if stats == hometown
+          binding.pry
           return contestant.values[3]
         end
       end
